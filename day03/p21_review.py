ae9ca7c70df2005e017cc4cbbedcfe0f9a857eaa
Legday <chlwodud0327@naver.com>
2024-02-23 13:30:39 +0900

    Feat: 파이썬의 입출력 리뷰 수정

--- day03/p21_review.py
+++ day03/p21_review.py
@@ -1,13 +1,18 @@
 # file: p21_review.py
 # desc: 파이썬의 입출력 리뷰
+import os
+
+os.system("clear")
 
 # Q7
 f = open("./day03/test.txt", mode="r", encoding="utf-8")
-body = f.readline()
+body = f.read()
 print(body)
+
 f.close()
 
 body = body.replace("java", "python")
+
 f = open("./day03/test.txt", mode="w", encoding="utf-8")
 f.write(body)
 f.close()
