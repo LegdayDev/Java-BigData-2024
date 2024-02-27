1aeaf1460adc4b4a56f0a15e79adc6647f442c1a
Legday <chlwodud0327@naver.com>
2024-02-27 11:49:01 +0900

    Feat: 주소록 앱 V1.3
    1. 콘솔창 정리 함수 추가
    2. 연락처 추가 시 리스트에 담기
    3. 연락처 출력 시 리스트에서 데이터 출력

--- day05/p34_addrBook.py
+++ day05/p34_addrBook.py
@@ -1,6 +1,8 @@
 # file: p34_addrBook.py
 # desc: 주소록 앱
 
+import os
+
 
 class Contact:  # 주소록 클래스
     __name = ""
@@ -19,19 +21,19 @@ class Contact:  # 주소록 클래스
 
 
 def setContact():  # 사용자 입력으로 주소록 받기 함수
-    (name, phoneNumber, eMail, addr) = input(
-        "주소록 입력(이름, 핸드폰, 이메일, 주소[구분자는 /]) >>"
-    ).split("/")
+    (name, phoneNumber, eMail, addr) = input("입력(이름/핸드폰/이메일/주소) >> ").split(
+        "/"
+    )
     name = name.strip()
     phoneNumber = phoneNumber.strip()
     eMail = eMail.strip()
     addr = addr.strip()
-    print(f'"{name}", "{phoneNumber}", "{eMail}", "{addr}"')
+    return Contact(name, phoneNumber, eMail, addr)
 
 
 def displayMenu():
     menu = (
-        "주소록 프로그램 V1\n"
+        "주소록 프로그램 V1.3\n"
         "1. 연락처 추가\n"
         "2. 연락처 출력\n"
         "3. 연락처 삭제\n"
@@ -39,14 +41,45 @@ def displayMenu():
     )
     print(menu)
     sel = int(input("메뉴 입력 >> "))
+
     return sel
 
 
+def clearConsole():
+    cmd = "clear"  # macOs, Linux, Unix 명령어
+    if os.name in ("nt", "dos"):  # Windows 명령어
+        cmd = "cls"
+    os.system(cmd)
+
+
+def getContact(listContact):
+    if len(listContact) == 0:
+        print("주소록이 비어있습니다.")
+    else:
+        for contact in listContact:
+            print(contact,'\n')
+
+
 def run():
+    # 연락처를 담을 리스트 생성
+    listContact = []
+    clearConsole()
     while True:
+        clearConsole()
         selMenu = displayMenu()
-        if selMenu == 4:
+
+        if selMenu == 1:  # 연락처 추가
+            listContact.append(setContact())
+            input("계속하시려면 Enter 키를 누르십시오")
+        elif selMenu == 2:  # 연락처 출력
+            getContact(listContact)
+            input("계속하시려면 Enter 키를 누르십시오")
+        elif selMenu == 3:  # 연락처 삭제
+            pass
+        elif selMenu == 4:  # 종료
             break
+        else:
+            continue
 
 
 if __name__ == "__main__":
