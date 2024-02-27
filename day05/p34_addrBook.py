91892b24b56f8625e56b728b4f3d09d38b515e48
Legday <chlwodud0327@naver.com>
2024-02-27 15:46:50 +0900

    Feat: 주소록 앱 V1.4
    1. 클래스 멤버함수 추가
      1) 객체가 특정 문자열을 갖고 있는지 검사
      2) 멤버필드를 튜플에 담아서 출력하는 함수
    2. 주소록 삭제함수 추가
    3. 프로그램 종료 시 저장했던 주소록 txt 파일로 저장하는 함수
    4. 메뉴 선택 후 함수 성공 메시지 출력
    5. 프로그램 재 실행시 주소록 txx파일에서 데이터를 가져와 리스트에 저장
    6. 예외처리 완료

--- day05/p34_addrBook.py
+++ day05/p34_addrBook.py
@@ -19,6 +19,15 @@ class Contact:  # 주소록 클래스
     def __str__(self) -> str:
         return f"이  름 = {self.__name}\n핸드폰 = {self.__phoneNumber}\n이메일 = {self.__eMail}\n주  소 = {self.__addr}"
 
+    def isNameExist(self, name):
+        if self.__name == name:
+            return True
+        else:
+            return False
+
+    def getInfo(self):
+        return self.__name, self.__phoneNumber, self.__eMail, self.__addr
+
 
 def setContact():  # 사용자 입력으로 주소록 받기 함수
     (name, phoneNumber, eMail, addr) = input("입력(이름/핸드폰/이메일/주소) >> ").split(
@@ -31,16 +40,61 @@ def setContact():  # 사용자 입력으로 주소록 받기 함수
     return Contact(name, phoneNumber, eMail, addr)
 
 
+def getContact(listContact):
+    if len(listContact) == 0:
+        print("주소록이 비어있습니다.")
+    else:
+        for i, contact in enumerate(listContact):
+            print(f"<<<<<<< {i+1} 번째 데이터 >>>>>>>")
+            print(contact, "\n")
+
+
+def delContact(list, name):
+    for i in range(len(list) - 1, -1, -1):  # 리스트를 내림차순으로 뒤에서부터 삭제
+        item = list[i]
+        if item.isNameExist(name):
+            del list[i]
+
+
+def saveContact(list):  # 연락처 저장 함수
+    with open("./contacts.txt", mode="w", encoding="utf-8") as fp:
+        for item in list:
+            name, phoneNumber, eMail, addr = item.getInfo()
+            fp.write(f"{name}/{phoneNumber}/{eMail}/{addr}\n")
+
+
+def loadContact(list):
+    try:
+        with open("./contacts.txt", mode="r", encoding="utf-8") as fp:
+            while True:
+                line = fp.readline()
+                if not line:
+                    break
+
+                lines = line.replace("\n", "").split("/")  # list
+                contact = Contact(
+                    name=lines[0], phoneNumber=lines[1], eMail=lines[2], addr=lines[3]
+                )
+                list.append(contact)
+    except:  # 연락처 파일이 없으면 빈 파일을 새로 만든다.
+        print("빈 파일 출력")
+        f = open("./contacts.txt", mode="w", encoding="utf-8")
+        f.close()
+
+
 def displayMenu():
     menu = (
-        "주소록 프로그램 V1.3\n"
+        "주소록 프로그램 V1.4\n"
         "1. 연락처 추가\n"
         "2. 연락처 출력\n"
         "3. 연락처 삭제\n"
         "4. 종료\n"
     )
     print(menu)
-    sel = int(input("메뉴 입력 >> "))
+    try:
+        sel = int(input("메뉴 입력 >> "))
+    except:
+        sel = 0
 
     return sel
 
@@ -52,31 +106,38 @@ def clearConsole():
     os.system(cmd)
 
 
-def getContact(listContact):
-    if len(listContact) == 0:
-        print("주소록이 비어있습니다.")
-    else:
-        for contact in listContact:
-            print(contact,'\n')
-
-
 def run():
     # 연락처를 담을 리스트 생성
     listContact = []
+    loadContact(listContact)  # 저장되어있던 연락처를 로딩한다.
     clearConsole()
     while True:
         clearConsole()
         selMenu = displayMenu()
 
         if selMenu == 1:  # 연락처 추가
-            listContact.append(setContact())
-            input("계속하시려면 Enter 키를 누르십시오")
+            clearConsole()
+            try:
+                contact = setContact()
+            except:  # 입력을 시킨대로 안하면
+                contact = None
+            if contact != None:
+                listContact.append(contact)
+                input("연락처 추가 성공!! Enter키를 누르십시오")
+            else:
+                input("연락처 입력 실패!! Enter키를 누르십시오")
         elif selMenu == 2:  # 연락처 출력
+            clearConsole()
             getContact(listContact)
-            input("계속하시려면 Enter 키를 누르십시오")
+            input("연락처 출력 성공!! Enter키를 누르십시오")
         elif selMenu == 3:  # 연락처 삭제
-            pass
+            clearConsole()
+            name = input("삭제할 이름 입력 >> ")
+            delContact(listContact, name)
+            input("연락처 삭제 성공!! Enter키를 누르십시오")
         elif selMenu == 4:  # 종료
+            print(f"list 길이는 {len(listContact)}")
+            saveContact(listContact)
             break
         else:
             continue
