fb8abf99a1a6fcbe7e88f4929e70fc0039e84c71
Legday <chlwodud0327@naver.com>
2024-03-04 14:54:29 +0900

    Feat: PyAutoGui 로 이미지 로딩하기

---
+++ day08/p53_imgLoad.py
@@ -0,0 +1,16 @@
+# file: p53_imgLoad.py
+
+import pyautogui as auto
+
+# capImg = auto.locateOnScreen('./images/day08Img/scrren.png')
+# print(capImg)
+# auto.click(capImg)
+
+# auto.alert("테스트", title="PyAutoGUI")
+# auto.confirm("계속 할꺼 ?")
+
+# text = auto.prompt("원하는 메시지 입력 >>")
+# print(text)
+
+# pwd = auto.password("암호 입력 >> ")
+# print(pwd)
